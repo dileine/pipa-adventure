4fb504beb495a654a5af92258487b0ce6a0b85f2
Mar LM <mar.audiovisuals@gmail.com>
2024-11-06 00:56:04 +0100

    update routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../components/home/Home";
+import CiutatVella from "../components/levels/CiutatVella";
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
+      <Route path='/pipa/CiutatVella' element={<CiutatVella />} />
     </Routes>
   );
 };
