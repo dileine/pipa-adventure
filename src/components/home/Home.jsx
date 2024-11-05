308b804201984db6c8f5bbbee756dcdec164b66c
Mar LM <mar.audiovisuals@gmail.com>
2024-11-06 00:55:40 +0100

    feat: create Home and CiutatVella components

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <h1>Benvingut a PiPa: Pisos i Paranys</h1>;
+};
+
+export default Home;
---
+++ src/components/levels/CiutatVella.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CiutatVella = () => {
+  return <h1>Benvignut a Ciutat Vella</h1>;
+};
+
+export default CiutatVella;
