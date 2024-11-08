b23b4e5f1dc4c244f7052d21a698a956936b5134
Mar LM <mar.audiovisuals@gmail.com>
2024-11-08 19:02:54 +0100

    Add CiutatVella component as first level

--- src/components/levels/CiutatVella.jsx
+++ src/components/levels/CiutatVella.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CiutatVella = () => {
-  return <h1>Benvignut a Ciutat Vella</h1>;
+  return (
+    <>
+      <h1>Benvingut a Ciutat Vella</h1>
+    </>
+  );
 };
 
 export default CiutatVella;
