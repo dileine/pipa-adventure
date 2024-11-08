a44f7c969da00fa29f1b7b1baf943f6458e54063
Mar LM <mar.audiovisuals@gmail.com>
2024-11-08 19:25:25 +0100

    feat: create useInventory with functions to manage inventory state

---
+++ src/hooks/useInventory.js
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import { PlayerContext } from "../contexts/player";
+
+export const useInventory = () => {
+  const { inventory, setInventory } = useContext(PlayerContext);
+
+  const addInventory = (items) => {
+    const inventoryMap = new Map(inventory);
+
+    for (const itemToAdd of items) {
+      const existingItem = inventoryMap.get(itemToAdd.id);
+
+      if (existingItem) {
+        existingItem.quantity += itemToAdd.quantity;
+      } else {
+        inventoryMap.set(itemToAdd.id, itemToAdd);
+      }
+    }
+    setInventory(inventoryMap);
+  };
+
+  const removeInventory = (itemToRemove) => {
+    setInventory(
+      inventory.filter((inventoryItem) => inventoryItem.id !== itemToRemove.id)
+    );
+  };
+
+  return { addInventory, removeInventory };
+};
