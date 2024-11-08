1914d8171d1ce2e50355a819188200be66eebb5e
Mar LM <mar.audiovisuals@gmail.com>
2024-11-08 19:23:11 +0100

    feat: create useGold hook with functions to manage gold state

---
+++ src/hooks/useGold.js
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { PlayerContext } from "../contexts/player";
+
+export const useGold = () => {
+  const { gold, setGold } = useContext(PlayerContext);
+
+  const increaseGold = (goldToIncrease) => {
+    setGold(gold + goldToIncrease);
+  };
+  const decreaseGold = (goldToDecrease) => {
+    goldToDecrease <= gold
+      ? setGold(gold - goldToDecrease)
+      : alert("No tens suficients pellofes");
+  };
+
+  return { increaseGold, decreaseGold };
+};
