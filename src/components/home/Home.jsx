00c09b8721319c1785855c9fbeca79d7f6ab1a41
Mar LM <mar.audiovisuals@gmail.com>
2024-11-08 18:59:06 +0100

    Add menu in Home, create StartScreen for intro, add Links for navigation and update routes in Router

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,22 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <h1>Benvingut!</h1>
-      <p>
-        <b>PiPa: Pisos i Paranys</b> és una aventura de text fantàstica i
-        satírica ambientada en la Barcelona actual. Ets un jove recent
-        desallotjat que emprèn una missió desesperada: trobar un pis en una
-        ciutat on els preus s'enfilen i els comerços tradicionals desapareixen.
-        Amb una forta crítica social i un toc d'humor sarcàstic, el joc revela
-        la realitat d'una ciutat en plena transformació, on els veïns lluiten
-        per sobreviure. En la recerca del teu lloc en aquest món, toparàs amb
-        éssers mitològics, paranys urbans i personatges històrics que
-        t'acompanyaran per un laberint urbà ple de sorpreses. Hauràs de superar
-        reptes, resoldre enigmes, enfrontar-te a enemics poderosos i prendre
-        decisions difícils per aconseguir una llar en aquesta ciutat hostil.
-      </p>
+      <h1>PiPa: Pisos i Paranys</h1>
+      <Link to='/pipa'>Start</Link>
+      <button>Settings</button>
+      <button>About</button>
     </>
   );
 };
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../components/home/Home";
 import CiutatVella from "../components/levels/CiutatVella";
+import StartScreen from "../components/screens/StartScreen";
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
+      <Route path='/pipa' element={<StartScreen />} />
       <Route path='/pipa/CiutatVella' element={<CiutatVella />} />
     </Routes>
   );
