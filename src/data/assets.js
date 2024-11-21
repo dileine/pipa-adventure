fe76c0b5da9ba87415b834ad12c09fa14f73e431
Mar LM <mar.audiovisuals@gmail.com>
2024-11-21 19:14:09 +0100

    Add asset imports and export icons object for inventory items

---
+++ src/data/assets.js
@@ -0,0 +1,9 @@
+import Cantimplora_img from "../assets/icons/cantimplora_cuir.png";
+import Carta_img from "../assets/icons/carta.png";
+import Quinque_img from "../assets/icons/quinque.png";
+
+export const icons = {
+  cantimplora: Cantimplora_img,
+  carta: Carta_img,
+  quinque: Quinque_img,
+};
