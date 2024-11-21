851ed96803de618abbac64b369bdd385f4c4725a
Mar LM <mar.audiovisuals@gmail.com>
2024-11-21 18:59:03 +0100

    Create dashboard folder

---
+++ src/components/dashboard/PlayerStats.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { GameContext } from "../../contexts/game";
+import { PlayerContext } from "../../contexts/player";
+
+const PlayerStats = () => {
+  const { currentLevel } = useContext(GameContext);
+  const { experience, health, gold } = useContext(PlayerContext);
+
+  return (
+    <>
+      <h2>Stats</h2>
+      <p>Nivell: {currentLevel}</p>
+      <p>Experiència: {experience}</p>
+      <p>Salut: {health}</p>
+      <p>Pellofes: {gold}</p>
+    </>
+  );
+};
+
+export default PlayerStats;
