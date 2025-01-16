de0e5fe18d90a1a52fcba4c784c3810e9dadd1b4
Mar LM <mar.audiovisuals@gmail.com>
2025-01-16 21:39:58 +0100

    Add PlayerScreen component with PlayerStats, Inventory popup, and quest list placeholder

--- src/components/screens/PlayerScreen.jsx
+++ src/components/screens/PlayerScreen.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import PlayerStats from "../dashboard/PlayerStats";
 import Inventory from "../dashboard/Inventory";
+import Popup from "../popup/popup";
 
 const PlayerScreen = () => {
+  const [popupContent, setPopupContent] = useState(null);
+  const [popupVisible, setPopupVisible] = useState(false);
+
+  const handleClickInventory = () => {
+    setPopupContent(<Inventory />);
+    setPopupVisible(true);
+  };
   return (
     <>
       <PlayerStats />
-      <Inventory />
+      <button onClick={handleClickInventory}>Inventory</button>
+      <Popup isVisible={popupVisible} onClose={() => setPopupVisible(false)}>
+        {popupContent}
+      </Popup>
       {/* quest list */}
     </>
   );
