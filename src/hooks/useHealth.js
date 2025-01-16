4e2d60abb8b243776cff2f7d131c72c3c0e9804e
Mar LM <mar.audiovisuals@gmail.com>
2025-01-16 22:17:40 +0100

    feat: create useHealth hook with functions to manage health state

---
+++ src/hooks/useHealth.js
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { PlayerContext } from "../contexts/PlayerContext";
+
+export const useHealth = () => {
+  const [health, setHealth] = useContext(PlayerContext);
+
+  const increaseHealth = (healthToIncrease) => {
+    if (health < 100) {
+      setHealth(health + healthToIncrease);
+    } else {
+      console.log("Health is already full");
+    }
+  };
+
+  const decreaseHealth = (healthToDecrease) => {
+    if (health > healthToDecrease) {
+      setHealth(health - healthToDecrease);
+    } else {
+      setHealth(0);
+      console.log("You died");
+    }
+  };
+
+  return { increaseHealth, decreaseHealth};
+};
