58bd944ee46fa2dd4444f90b1005538246af5226
Mar LM <mar.audiovisuals@gmail.com>
2025-01-16 22:37:08 +0100

    feat: Add levels to game configuration in game.json and Update LevelScreen component to display current level name

--- src/components/screens/LevelScreen.jsx
+++ src/components/screens/LevelScreen.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GameContext } from "../../contexts/game";
+import game from "../../data/game.json";
 
 const LevelScreen = () => {
+  const { currentLevel } = useContext(GameContext);
+  const levelName = game.levels[currentLevel] || "Nivell desconegut";
+
   return (
     <>
-      <h1>Nom nivell</h1>
+      <h1>{levelName}</h1>
     </>
   );
 };
