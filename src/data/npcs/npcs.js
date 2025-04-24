c1d599eb3883d89fbdc9b8bfdd73eaca1d6dd3ff
Mar LM <mar.audiovisuals@gmail.com>
2025-04-24 21:19:04 +0200

    fix: correct imports

--- src/data/npcs/npcs.js
+++ src/data/npcs/npcs.js
@@ -1,15 +1,15 @@
-import { NPC } from "../NPC.js";
+import { NPC } from "../classes/npc.js";
 
 //Types for NPCS: inform, vendor, quest-giver, enemy, friend, companion, ally.
 
 export const Ona = new NPC(
   "Ona",
-  "",
+  "inform",
   "Encantèria Gitana",
   "", // onaImage // Placeholder for image path
   "plaça Sant Just",
   {
-    Desnonament1: `"Bon dia ${playerName}, veig que al final a tú també t'han fet fora de casa..."`,
+    Desnonament1: `"Bon dia PLAYER_NAME, veig que al final a tú també t'han fet fora de casa..."`,
     Desnonament2: `"És tota una aventura trobar una llar en aquesta ciutat..."`,
   },
   {}
