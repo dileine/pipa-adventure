8bc9a1b09c1984209a4d2469ed8c179e347a6a1f
Mar LM <mar.audiovisuals@gmail.com>
2025-07-28 14:04:47 +0200

    Move inventory component to iventory folder

---
+++ src/components/screens/inventoryScreen/Inventory.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { PlayerContext } from "../../../contexts/player";
+import { icons } from "../../../assets/assets";
+
+const Inventory = () => {
+  const { inventory } = useContext(PlayerContext);
+  console.log("Inventory:", inventory);
+
+  return (
+    <>
+      <h2>Inventari</h2>
+      {Array.from(inventory.entries()).map(([item, quantity]) => (
+        <div key={item}>
+          {icons[item] && <img src={icons[item]} alt={item} />}
+          {quantity > 1 && <span>x{quantity}</span>}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Inventory;
