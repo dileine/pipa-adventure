f639658e1511f8edad8b0c3386e0e7adcad2741a
Mar LM <mar.audiovisuals@gmail.com>
2025-07-28 14:02:53 +0200

    Update icons files

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,20 @@
 import Cantimplora_img from "../assets/icons/cantimplora_cuir.png";
 import Carta_img from "../assets/icons/carta.png";
 import Quinque_img from "../assets/icons/quinque.png";
+import pellofes_img from "../assets/icons/gold.png";
+import brain_img from "../assets/icons/brain.png";
+import gotic_1 from "../assets/images/barri-gotic.jpeg";
 
 export const icons = {
   cantimplora: Cantimplora_img,
   carta: Carta_img,
   quinque: Quinque_img,
+  pellofes: pellofes_img,
+  health: brain_img,
+  level: "",
+  experience: "",
+};
+
+export const images = {
+  gotic: gotic_1,
 };
