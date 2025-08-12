e3ac79611caa49b606ca5f5775542140386666d5
Mar LM <mar.audiovisuals@gmail.com>
2025-08-12 20:07:49 +0200

    feat: add some shadow color to popup styles

--- src/components/popup/Popup.styled.jsx
+++ src/components/popup/Popup.styled.jsx
@@ -6,7 +6,7 @@ export const PopupWrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.29);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -17,19 +17,17 @@ export const PopupInner = styled.div`
   position: relative;
   padding: 10rem;
   width: 50%;
-  background-color: rgba(47, 161, 60, 0.2);
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  background-color: rgba(36, 243, 60, 0.32);
+  box-shadow: 0px 0px 10px 0px rgba(32, 119, 191, 0.38);
   border-radius: 10px;
-
   border-radius: 3rem;
 `;
 
 export const CloseButton = styled.button`
   position: absolute;
-  top: 10px;
-  right: 10px;
+  top: 15px;
+  right: 20px;
   background: transparent;
   border: none;
-  font-size: 3rem;
   cursor: pointer;
 `;
